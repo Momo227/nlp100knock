b26d031a12f61e1fa02aed4e3281dea8fa7baacb
Natsuki NOGUCHI <61526498+Momo227@users.noreply.github.com>
2021-02-16 22:38:59 +0900

    add q00.py

---
+++ src/chapter01/q00.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- Coding: utf-8 -*-
+
+def re_value(a):
+    # 文字列を逆に出力する
+    re_a_list = list(reversed(a))
+
+    # listにて表示
+    print(re_a_list)
+
+    # ''と空白を削除
+    re_a = ''.join(list(reversed(a)))
+
+    return re_a
+
+
+def main():
+    value = re_value("stress")
+    print(value)
+
+
+if __name__ == '__main__':
+    main()
