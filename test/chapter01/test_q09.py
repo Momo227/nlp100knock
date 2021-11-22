dd01c31ffef816848a7e5ad6f216f2a7022233e4
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2021-11-22 21:27:57 +0900

    update question_09

--- test/chapter01/test_q09.py
+++ test/chapter01/test_q09.py
@@ -3,9 +3,9 @@
 import sys
 
 sys.path.append("../../")
-from src.chapter01 import q08
+from src.chapter01 import q09
 
 
 def test_re_value():
-    assert q08.cipher('Oh, I have had such a curious dream!') == "Os, I szev szw hfxs z xfirlfh wivzn!"
-    assert q08.cipher('Wake up, Alice dear! Why, what a long sleep you have had!') == "Wzpv fk, Aorxv wvzi! Wsb, dszg z olmt hovvk blf szev szw!"
+    assert q09.typoglycemia("I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind.") == ['I', 'the', 'couldn’t', 'phenomenal', 'what', 'was', 'actually', 'human', 'understand', ':', 'that', 'could', 'believe', 'power', 'I', 'reading', 'I', 'of', 'the', 'mind.']
+    assert q09.typoglycemia('I like dogs.') == ["I", "like", "dogs."]
