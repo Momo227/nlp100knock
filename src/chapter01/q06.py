bdce1aae2e9c0afa1bccc926ee6c370eb368fe48
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2021-11-22 21:29:17 +0900

    update question_06

--- src/chapter01/q06.py
+++ src/chapter01/q06.py
@@ -1,18 +1,3 @@
-#!/usr/bin/python
-# -*- Coding: utf-8 -*-
-
-def word_bi_gram(x):
-    c = x.replace(',', '').replace('.', '')
-    c = c.split()
-    ans = []
-    for i in range(len(c) - 1):
-        ans.append(c[i:i+2])
-
-    return ans
-        
-
-
-
 def char_bi_gram(x):
     c = x.replace(',', '').replace('.', '')
     c = c.split()
@@ -48,13 +33,78 @@ def char_bi_gram(x):
 
     return ans
 
+def make_wa(x, y):
+    ans = []
 
+    for i in range(len(x)):
+        if x[i] not in ans:
+            ans.append(x[i])
+        else:
+            continue
+
+    for i in range(len(y)):
+        if y[i] not in ans:
+            ans.append(y[i])
+        else:
+            continue
+
+    return ans
+
+def make_seki(x, y):
+    ans = []
+
+    for i in range(len(x)):
+        if (x[i] in x) and (x[i] in y):
+            if x[i] not in ans:
+                ans.append(x[i])
+            else:
+                continue
+        else:
+            continue
+
+    for i in range(len(y)):
+        if (y[i] in x) and (y[i] in y):
+            if y[i] not in ans:
+                ans.append(y[i])
+            else:
+                continue
+        else:
+            continue
+
+    return ans
+
+def make_sa(x, y):
+    x = set(map(tuple, x))
+    y = set(map(tuple, y))
+    ans = x.difference(y)
+
+    return ans
 
 def main():
-    ans1 = word_bi_gram("I am an NLPer")
-    ans2 = char_bi_gram("I am an NLPer")
-    print("word_bi_gram : " + str(ans1) + "\n")
-    print("char_bi_gram : " + str(ans2) + "\n")
+    X = char_bi_gram("paraparaparadise")
+    Y = char_bi_gram("paragraph")
+
+
+    wa = make_wa(X, Y)
+    print("和集合 : " + str(wa) + "\n")
+
+    seki = make_seki(X, Y)
+    print("積集合 : " + str(seki) + "\n")
+
+    sa = make_sa(X, Y)
+    print("差集合 : " + str(sa) + "\n")
+
+    se = char_bi_gram("se")
+
+    if se[0] in X:
+        print("X:Yes")
+    else:
+        print("X:No")
+
+    if se[0] in Y:
+        print("Y:Yes")
+    else:
+        print("Y:No")
 
 
 if __name__ == '__main__':
