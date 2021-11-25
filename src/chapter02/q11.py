06e18baaba71d76209b6d82ea915fdce06c38352
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2021-11-25 11:16:12 +0900

    update q11.py

--- src/chapter02/q11.py
+++ src/chapter02/q11.py
@@ -1,12 +1,21 @@
 import pandas as pd
+import csv
 
 def load_data(file_path):
-    data = pd.read_csv(file_path, sep='\t')
-    return data
+    rows = []
+    with open(file_path) as f:
+        for row in f:
+            row = row.strip().replace("\t"," ")
+            rows.append(row)
+
+    return rows
+
 
 def main():
     data = load_data("../../data/popular-names.txt")
-    print(len(data))
+
+    print(data)
+
 
 
 if __name__ == '__main__':
