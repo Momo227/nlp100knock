cd7582582e34af3554c996c3e14dd39ea0e67975
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2021-11-29 19:59:07 +0900

    add q17.py

--- src/chapter02/q17.py
+++ src/chapter02/q17.py
@@ -1,39 +1,16 @@
-import sys
-def load_data(file_path):
-    rows = []
-    with open(file_path) as f:
-        for row in f:
-            row = row.strip().replace("\t"," ")
-            rows.append(row)
-
-    return rows
+import pandas as pd
 
 def main():
-    args = sys.argv
+    data = pd.read_csv("../../data/popular-names.txt", delimiter='\t', names=['name', "data1", "data2", "data3"])
+    name = data["name"].to_list()
 
-    num = args[1]
+    name = set(name)
 
-    num = int(num)
+    print(len(name))
 
-    data = load_data("../../data/popular-names.txt")
-
-    zenhan = []
-    for i in range(num):
-        zenhan.append(data[i])
-
-    kouhan = []
-    for i in range(num, len(data)):
-        kouhan.append(data[i])
-
-    print("前半\n")
-    print(zenhan)
-    print("\n後半\n")
-    print(kouhan)
 
 if __name__ == '__main__':
     main()
-
-# python q15.py N (Nに任意の自然数)
 
 
 
