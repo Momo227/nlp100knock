665baa71fc2d44bbd9ab16019c21263477c5c68e
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2021-11-29 15:28:21 +0900

    add q12.py

--- src/chapter02/q12.py
+++ src/chapter02/q12.py
@@ -1,22 +1,10 @@
 import pandas as pd
-import csv
-
-def load_data(file_path):
-    rows = []
-    with open(file_path) as f:
-        for row in f:
-            row = row.strip().replace("\t"," ")
-            rows.append(row)
-
-    return rows
-
 
 def main():
-    data = load_data("../../data/popular-names.txt")
+    data = pd.read_csv("../../data/popular-names.txt", delimiter='\t', names=['name', 'sex', 'data1', 'data2'])
 
-    print(data)
-
-
+    data.to_csv('../../data/col1.txt', columns=['name'], header=None, index=False)
+    data.to_csv('../../data/col2.txt', columns=['sex'], header=None, index=False)
 
 if __name__ == '__main__':
     main()
