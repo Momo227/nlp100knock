861ed39e9b42bb817c9e27d5b1a1b6b9487873df
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2021-11-30 14:40:16 +0900

    add question_19

--- src/chapter02/q19.py
+++ src/chapter02/q19.py
@@ -3,7 +3,7 @@ import pandas as pd
 def main():
     data = pd.read_csv("../../data/popular-names.txt", delimiter='\t', names=['name', "sex", "data", "year"])
 
-    sorted_data = data.sort_values('data', ascending=False)
+    sorted_data = data['name'].value_counts(sort=True)
     print(sorted_data)
 
 
