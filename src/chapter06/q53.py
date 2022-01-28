2186cf2fe0719540b3d4893bbfbbb3b06188c362
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2022-01-28 19:24:40 +0900

    add q53.py and update q56.py

--- src/chapter06/q53.py
+++ src/chapter06/q53.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 
@@ -7,14 +6,12 @@ def main():
     train = pd.read_csv("../../data/NewsAggregatorDataset/train.feature.txt", sep="\t")
     train_test = pd.read_csv("../../data/NewsAggregatorDataset/train.txt", sep="\t")
     test = pd.read_csv("../../data/NewsAggregatorDataset/test.feature.txt", sep="\t")
-    test_test = pd.read_csv("../../data/NewsAggregatorDataset/test.txt", sep="\t")
 
     train_test = train_test['category'].map({'e': 0, 'b': 1, 'm': 2, 't': 3})
 
     clf = LogisticRegression(random_state=2, max_iter=10000)
     clf.fit(train, train_test)
 
-    # Probability estimates.
     print(clf.predict_proba(test))
     print(clf.predict(test))
 
