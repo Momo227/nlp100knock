64449c44f305a63333abd7df6857503eff5febfc
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2022-02-03 18:10:01 +0900

    add q47.py

--- src/chapter05/q47.py
+++ src/chapter05/q47.py
@@ -65,25 +65,23 @@ def main():
             for chunk in sentence.chunks:
                 for morph in chunk.morphs:
                     if morph.pos == '動詞':
+                        # インデックスも取得
                         for i, src in enumerate(chunk.srcs):
-                            if len(sentence.chunks[src].morphs) == 2 and sentence.chunks[src].morphs[
-                                0].pos1 == 'サ変接続' and sentence.chunks[src].morphs[1].surface == 'を':
-                                predicate = ''.join(
-                                    [sentence.chunks[src].morphs[0].surface, sentence.chunks[src].morphs[1].surface,
-                                     morph.base])
+                            # サ変接続名詞+を（助詞）の2つで形態素解析されているのものを抽出
+                            if len(sentence.chunks[src].morphs) == 2 and sentence.chunks[src].morphs[0].pos1 == 'サ変接続' and sentence.chunks[src].morphs[1].surface == 'を':
+                                predicate = ''.join([sentence.chunks[src].morphs[0].surface, sentence.chunks[src].morphs[1].surface, morph.base])
                                 cases = []
-                                modi_chunks = []
-                                for src_r in chunk.srcs[:i] + chunk.srcs[i + 1:]:  # 残りの係り元chunkから助詞を探す
-                                    case = [morph.surface for morph in sentence.chunks[src_r].morphs if
-                                            morph.pos == '助詞']
+                                sahen = []
+                                # 今のインデックス以外の係り元chunkから助詞を探す
+                                for kakarisaki_id in chunk.srcs[:i] + chunk.srcs[i + 1:]:
+                                    case = [morph.surface for morph in sentence.chunks[kakarisaki_id].morphs if morph.pos == '助詞']
                                     if len(case) > 0:  # 助詞を含むchunkの場合は助詞と項を取得
+                                        # 助詞はあるだけ書く
                                         cases = cases + case
-                                        modi_chunks.append(''.join(
-                                            morph.surface for morph in sentence.chunks[src_r].morphs if
-                                            morph.pos != '記号'))
+                                        sahen.append(''.join(morph.surface for morph in sentence.chunks[kakarisaki_id].morphs if morph.pos != '記号'))
                                 if len(cases) > 0:  # 助詞が1つ以上見つかった場合は重複除去後辞書順にソートし、項と合わせて出力
                                     cases = sorted(list(set(cases)))
-                                    line = '{}\t{}\t{}'.format(predicate, ' '.join(cases), ' '.join(modi_chunks))
+                                    line = '{}\t{}\t{}'.format(predicate, ' '.join(cases), ' '.join(sahen))
                                     print(line, file=f)
                                 break
 
