36909532a74343eb1c69b08d372e77a662551434
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2022-02-03 19:15:14 +0900

    add q48.py, q49.py

--- src/chapter05/q49.py
+++ src/chapter05/q49.py
@@ -1,3 +1,7 @@
+from itertools import combinations
+import re
+
+
 class Morph:
     def __init__(self, morph):
         (surface, attr) = morph.split("\t")
@@ -52,18 +56,61 @@ def main():
 
     # 追加
     sentence = sentences[2]
-
-    for chunk in sentence.chunks:
-        # 形態素が名詞
+    nouns = []
+    # インデックスも作成
+    for i, chunk in enumerate(sentence.chunks):
+        # 名詞を含む文節のインデックスを抽出
         if '名詞' in [morph.pos for morph in chunk.morphs]:
-            # 文内で、記号以外を取得
-            words = [''.join(morph.surface for morph in chunk.morphs if morph.pos != '記号')]
-            # 名詞を含むchunkを先頭に、係り先文節インデックス番号（dst）を根まで順に辿ってリストに追加
-            # -1 = 文末
-            while chunk.dst != -1:
-                words.append(''.join(morph.surface for morph in sentence.chunks[chunk.dst].morphs if morph.pos != '記号'))
-                chunk = sentence.chunks[chunk.dst]
-            print(' -> '.join(words))
+            nouns.append(i)
+    # 名詞を含む文節のペアごとにパスを作成
+    for i, j in combinations(nouns, 2):
+        path_i = []
+        path_j = []
+        # パス作成中　同じ数＝末端になったら終了
+        while i != j:
+            if i < j:
+                path_i.append(i)
+                i = sentence.chunks[i].dst
+            else:
+                path_j.append(j)
+                j = sentence.chunks[j].dst
+
+        # 文節iから構文木の根に至る経路上に文節jが存在する場合: 文節iから文節jのパスを表示について
+        # i の係り受け先に j がある
+        if len(path_j) == 0:
+            # 名詞以外だったら pass、名詞はX or Y に置換
+            # X -> 先頭
+            # Y -> かかり先
+            chunk_X = ''.join(
+                [morph.surface if morph.pos != '名詞' else 'X' for morph in sentence.chunks[path_i[0]].morphs])
+            chunk_Y = ''.join([morph.surface if morph.pos != '名詞' else 'Y' for morph in sentence.chunks[i].morphs])
+
+            # X や Y が複数あったら1つに
+            chunk_X = re.sub('X+', 'X', chunk_X)
+            chunk_Y = re.sub('Y+', 'Y', chunk_Y)
+
+            # X (先頭) + 文節（中間）　+ Y (文末)
+            path_XtoY = [chunk_X] + [''.join(morph.surface for morph in sentence.chunks[n].morphs) for n in
+                                     path_i[1:]] + [chunk_Y]
+            print(' -> '.join(path_XtoY))
+        # 上記以外で，文節iと文節jから構文木の根に至る経路上で共通の文節kで交わる場合: 文節iから文節kに至る直前のパスと文節jから文節kに至る直前までのパス，文節kの内容を” | “で連結して表示
+        else:
+            chunk_X = ''.join(
+                [morph.surface if morph.pos != '名詞' else 'X' for morph in sentence.chunks[path_i[0]].morphs])
+            chunk_Y = ''.join(
+                [morph.surface if morph.pos != '名詞' else 'Y' for morph in sentence.chunks[path_j[0]].morphs])
+            # 共通文節
+            chunk_k = ''.join([morph.surface for morph in sentence.chunks[i].morphs])
+
+            chunk_X = re.sub('X+', 'X', chunk_X)
+            chunk_Y = re.sub('Y+', 'Y', chunk_Y)
+
+            # かかり関係のリスト
+            path_X = [chunk_X] + [''.join(morph.surface for morph in sentence.chunks[n].morphs) for n in path_i[1:]]
+            path_Y = [chunk_Y] + [''.join(morph.surface for morph in sentence.chunks[n].morphs) for n in path_j[1:]]
+
+            # リストを -> で接続　→　共通するものを | で接続
+            print(' | '.join([' -> '.join(path_X), ' -> '.join(path_Y), chunk_k]))
 
 
 if __name__ == '__main__':
