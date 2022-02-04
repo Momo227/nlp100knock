5bb0a5af8fcbc61f58d48643a05b28907c2afdd4
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2022-02-04 12:57:34 +0900

    add q90.py

--- src/chapter10/q90.py
+++ src/chapter10/q90.py
@@ -6,9 +6,9 @@ def main():
             "../../data/kyoto-train.en") as train_x, open(
             "../../data/kyoto-train.ja") as train_y, open(
             "../../data/kyoto-dev.en") as valid_x, open(
-            "../../data/kyoto-dev.ja.txt") as valid_y, open(
+            "../../data/kyoto-dev.ja") as valid_y, open(
             "../../data/kyoto-test.en") as test_x, open(
-            "../../data/kyoto-test.ja.txt") as test_y:
+            "../../data/kyoto-test.ja") as test_y:
 
 
         train_X = train_x.read()
