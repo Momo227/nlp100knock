d0026c7c1c1c03eb0bb3fe49f992b0ef65e2adb4
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2022-02-04 15:18:24 +0900

    update q90.py

--- src/chapter10/q90.py
+++ src/chapter10/q90.py
@@ -2,24 +2,30 @@ from transformers import MarianMTModel
 from transformers.models.marian.tokenization_marian import MarianTokenizer
 def main():
     print("load_data")
+    # with open(
+    #         "../../data/kyoto-train.en") as train_x, open(
+    #         "../../data/kyoto-train.ja") as train_y, open(
+    #         "../../data/kyoto-dev.en") as valid_x, open(
+    #         "../../data/kyoto-dev.ja") as valid_y, open(
+    #         "../../data/kyoto-test.en") as test_x, open(
+    #         "../../data/kyoto-test.ja") as test_y:
+    #
+    #
+    #     train_X = train_x.read()
+    #     train_Y = train_y.read()
+    #     valid_X = valid_x.read()
+    #     valid_Y = valid_y.read()
+    #     test_X = test_x.read()
+    #     test_Y = test_y.read()
+
     with open(
-            "../../data/kyoto-train.en") as train_x, open(
-            "../../data/kyoto-train.ja") as train_y, open(
-            "../../data/kyoto-dev.en") as valid_x, open(
-            "../../data/kyoto-dev.ja") as valid_y, open(
-            "../../data/kyoto-test.en") as test_x, open(
-            "../../data/kyoto-test.ja") as test_y:
-
-
+            "../../data/mini.en") as train_x, open(
+        "../../data/mini.ja") as train_y:
         train_X = train_x.read()
         train_Y = train_y.read()
-        valid_X = valid_x.read()
-        valid_Y = valid_y.read()
-        test_X = test_x.read()
-        test_Y = test_y.read()
 
     print("make list of test")
-    src_text = [sentence for sentence in test_X.split("\n")]
+    src_text = [sentence for sentence in train_X.split("\n")]
 
 
     print("translate")
